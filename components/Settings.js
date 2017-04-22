5d5341881d5c0184b2adb3f3d4206c451541ee61
Richie Messina <messina.rd@gmail.com>
2017-04-22 17:46:56 -0400

    fix styling some more

--- components/Settings.js
+++ components/Settings.js
@@ -9,14 +9,14 @@ import {
 } from 'react-native';
 import { CheckboxGroup } from 'react-native-material-design';
 import { MKCheckbox } from 'react-native-material-kit';
+import { Col, Row, Grid } from "react-native-easy-grid";
 
 class Settings extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      saved: [ false, false, false ]
-	  
+      saved: [ false, false, false, false, false, false ]
     };
 
     this._renderCheckboxes = this._renderCheckboxes.bind(this);
@@ -27,116 +27,84 @@ class Settings extends Component {
       this.setState({ saved: (value || '').split(',').map((val) => (val === 'true')) });
     }).done();
   }
-  
 
   _renderCheckboxes() {
     const options = [
       { label: 'Vegan' },
       { label: 'Vegetarian' },
       { label: 'Nut Allergy' },
-	  {label: 'Sugar-free'},
-	  {label: 'Gluten-free'},
-	  {label: 'Lactose intolerance'},
+  	  { label: 'Sugar-free' },
+  	  { label: 'Gluten-free' },
+  	  { label: 'Lactose Intolerance' },
     ];
 
     let self = this;
     return options.map((item, i) => {
       return (
-        <View key={i}>
-          <MKCheckbox
-            checked={self.state.saved[i]}
-            onCheckedChange={({checked}) => {
-              self.setState({
-                saved: [
+        <Row key={i}>
+          <Col size={1}>
+            <MKCheckbox
+              checked={self.state.saved[i]}
+              onCheckedChange={({checked}) => {
+                self.setState({
+                  saved: [
+                    ...self.state.saved.slice(0, i),
+                    checked,
+                    ...self.state.saved.slice(i + 1)
+                  ]
+                });
+                AsyncStorage.setItem('savedSettings', [
                   ...self.state.saved.slice(0, i),
                   checked,
                   ...self.state.saved.slice(i + 1)
-                ]
-              });
-              AsyncStorage.setItem('savedSettings', [
-                ...self.state.saved.slice(0, i),
-                checked,
-                ...self.state.saved.slice(i + 1)
-              ].toString())
-            }}
-          /><Text style={styles.bodyfont}>{item.label}</Text>
-        </View>
+                ].toString())
+              }}
+            />
+          </Col>
+          <Col size={7}><Text style={styles.bodyfont}>{item.label}</Text></Col>
+        </Row>
       );
     });
   }
 
   render() {
     return (
-		<View style={styles.container}>
-		<Image
-		  style = {styles.container}
-          source={require('./lemon.jpg')}>
-		<View style={{width: 300, height: 500, backgroundColor: '#f5f5f5', alignSelf: 'center', paddingTop:30, marginTop:60}} >	
-			<Text style ={styles.heading}>Select your dietary restrictions {'\n'}</Text>
-			<Text>{'\n'}</Text>  
-			{this._renderCheckboxes()}
-			<Text>{'\n'}{'\n'}{'\n'}</Text>
-		</View>
-		<Text>{'\n'}</Text>
-		<Button
+      <View style={styles.container}>
+          <Text style ={styles.heading}>Select your dietary restrictions:</Text>
+          <Grid>
+            {this._renderCheckboxes()}
+          </Grid>
+        <Button
           onPress={() => this.props.changePage('SCANNER')}
           title="Return to Scanner"
           accessibilityLabel="press this to return to the camera"
-		  color = '#32cd32'
+          color = '#32cd32'
         />
-		
-		</Image>
       </View>
     );
   }
 }
-
-
-export default Settings;
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: undefined,
     height: undefined,
-    backgroundColor:'transparent',
-       
+    backgroundColor: '#f5f5f5',
+    padding: 30
   },
   heading: {
     color: '#32cd32',
-	fontSize: 20,
-	fontFamily: 'Chalkduster',
-	fontWeight: 'bold',
-		
+    fontSize: 20,
+    fontFamily: 'Chalkduster',
+    fontWeight: 'bold'
   },
   bodyfont: {
     color: '#32cd32',
-	fontSize: 17,
-	fontFamily: 'Chalkduster',
-	fontWeight: 'bold',
-	
-  }, 
-  newimg:{
-	  width:300,
-	  height:650,
-	  resizeMode: 'center',
-	  alignItems: 'center',
-	  justifyContent: 'center',
-	  alignSelf: 'center',
-  },
-  buttonstyle: {
-	color: 'white',
-    textAlign: 'center',
-    fontSize: 16,
-	width: 50,
-  },
-  stretch: {
-    width: 400,
-    height: 100,
-	resizeMode: 'contain',
-	alignItems: 'center',
-	justifyContent: 'center',
-	alignSelf: 'center',    
-  },
+    fontSize: 17,
+    fontFamily: 'Chalkduster',
+    fontWeight: 'bold'
+  }
 });
+
+export default Settings;
